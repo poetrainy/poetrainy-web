67c5dc1ab00d344ebaa0930d93f148c5b29c9c53
manami-sato <manamisato2001@gmail.com>
2023-06-14 22:03:16 +0900

    Fix/OGPが反映されない問題を修正

--- src/components/HeadOgp.tsx
+++ src/components/HeadOgp.tsx
@@ -1,27 +1,24 @@
 import { FC } from 'react';
-import Head from 'next/head';
 
 import { NAME, SITE_TITLE_FULL, SITE_URL } from '@/constants/common';
 
-const HeadOgp: FC = () => {
-  return (
-    <Head>
-      <title>{SITE_TITLE_FULL}</title>
-      <meta property={'og:title'} content={SITE_TITLE_FULL} />
-      <meta property={'og:url'} content={SITE_URL} />
-      <meta
-        property={'og:description'}
-        content={'写真を撮ることが好きな新米Webエンジニアです。'}
-      />
-      {/* ページの種類 */}
-      <meta property={'og:type'} content={'website'} />
-      {/* サイト名 */}
-      <meta property={'og:site_name'} content={NAME} />
-      {/* サムネイル画像の URL */}
-      <meta property={'og:image'} content={`${SITE_URL}/img/icon.jpg`} />
-      <meta name={'twitter:card'} content={'summary'} />
-    </Head>
-  );
-};
+const HeadOgp: FC = () => (
+  <>
+    <title>{SITE_TITLE_FULL}</title>
+    <meta property={'og:title'} content={SITE_TITLE_FULL} />
+    <meta property={'og:url'} content={SITE_URL} />
+    <meta
+      property={'og:description'}
+      content={'写真を撮ることが好きな新米Webエンジニアです。'}
+    />
+    {/* ページの種類 */}
+    <meta property={'og:type'} content={'website'} />
+    {/* サイト名 */}
+    <meta property={'og:site_name'} content={NAME} />
+    {/* サムネイル画像の URL */}
+    <meta property={'og:image'} content={`${SITE_URL}/img/icon.jpg`} />
+    <meta name={'twitter:card'} content={'summary'} />
+  </>
+);
 
 export default HeadOgp;
