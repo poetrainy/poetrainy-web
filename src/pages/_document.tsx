7e5b048df698ea0ef8bbb0c214f5eaf9c411615a
manami-sato <manamisato2001@gmail.com>
2023-06-14 22:03:35 +0900

    Fix/OPGが反映されない問題を修正

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,9 +15,9 @@ export default class Document extends NextDocument {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="theme-color" content="#fff" />
+          <HeadOgp />
         </Head>
         <body>
-          <HeadOgp />
           {/* <ColorModeScript /> */}
           <Main />
           <NextScript />
