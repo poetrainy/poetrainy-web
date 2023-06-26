782cf4e18b4ec12c71fbfbab4bac4f67392500bd
manami-sato <manamisato2001@gmail.com>
2023-06-26 18:44:18 +0900

    Feature/[/archive] 実装

--- src/components/ButtonSmall.tsx
+++ src/components/ButtonSmall.tsx
@@ -14,26 +14,38 @@ const ButtonSmall: FC<Props> = ({ text, isLangJp, isLarge, onClick }) => (
     as={'button'}
     onClick={() => onClick()}
     alignItems={'center'}
-    w={isLarge ? '100%' : '104px'}
-    h={'40px'}
+    w={isLarge ? '80%' : '104px'}
+    h={isLarge ? '72px' : '40px'}
     color={'white'}
     bg={'green'}
     fontFamily={'sp'}
     p={isLarge ? '3px 0 0' : '3px 0 0 12px'}
     transition={'opacity 0.2s'}
+    pos={'relative'}
     opacity={1}
     _hover={{
       opacity: 0.7,
     }}
     sx={{
       ...(isLarge && {
-        justifyContent: 'center',
+        // justifyContent: 'center',
+        pl: '5%',
+        maxW: '600px',
+        m: 'auto',
+        fontSize: '2rem',
+        '&::before': {
+          content: '""',
+          display: 'block',
+          w: '80px',
+          h: '24px',
+          bg: 'url("/img/bunner-arrow.svg") no-repeat',
+          bgSize: 'contain',
+          bgPosition: 'center',
+          opacity: 0.7,
+          pos: 'absolute',
+          inset: 'auto 10% auto auto',
+        },
       }),
-      ...(isLangJp
-        ? {}
-        : {
-            fontSize: '1.8rem',
-          }),
     }}
   >
     {text}
--- src/components/Home/Icon.tsx
+++ src/components/Home/Icon.tsx
@@ -1,38 +1,64 @@
 import { FC } from 'react';
 import { Center } from '@chakra-ui/react';
+import NextLink from 'next/link';
 
 const Icon: FC = () => (
-  <Center
-    w={'240px'}
-    h={'240px'}
-    bg={'black800'}
-    borderRadius={'9999px'}
-    overflow={'hidden'}
-    pos={'relative'}
-    sx={{
-      '&::before': {
-        content: "''",
-        display: 'block',
-        w: '50%',
-        h: '100%',
-        pos: 'absolute',
-        bg: 'green',
-        inset: '0 auto auto 0',
-        transform: 'rotateZ(30deg)',
-        transformOrigin: 'right',
-      },
-    }}
-  >
+  <NextLink href={'/archive'} passHref>
     <Center
-      w={'224px'}
-      h={'224px'}
+      as={'a'}
+      w={'240px'}
+      h={'240px'}
+      bg={'black800'}
       borderRadius={'9999px'}
-      bg={'url("/img/icon.jpg") no-repeat'}
-      bgSize={'contain'}
       overflow={'hidden'}
-      zIndex={5}
-    />
-  </Center>
+      pos={'relative'}
+      _hover={{
+        "&::after":{
+          opacity: 1,
+        }
+      }}
+      sx={{
+        '&::before': {
+          content: "''",
+          display: 'block',
+          w: '50%',
+          h: '100%',
+          pos: 'absolute',
+          bg: 'green',
+          inset: '0 auto auto 0',
+          transform: 'rotateZ(30deg)',
+          transformOrigin: 'right',
+        },
+        '&::after': {
+          content: "'💻🤍'",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          w: '224px',
+          h: '224px',
+          bg: '#ffffff99',
+          fontSize: '3rem',
+          opacity: 0,
+          transition: 'opacity 0.1s',
+          pos: 'absolute',
+          inset: '8px 8px auto auto',
+          borderRadius: '9999px',
+          zIndex: 2,
+          textShadow: '0 0 10px #00000090',
+        },
+      }}
+    >
+      <Center
+        w={'224px'}
+        h={'224px'}
+        borderRadius={'9999px'}
+        bg={'url("/img/icon.jpg") no-repeat'}
+        bgSize={'contain'}
+        overflow={'hidden'}
+        zIndex={1}
+      />
+    </Center>
+  </NextLink>
 );
 
 export default Icon;
