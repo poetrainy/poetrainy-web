42ff28ccce577a21b8e37fc1c5cb31d1fcc6f153
manami-sato <manamisato2001@gmail.com>
2023-07-08 03:38:27 +0900

    fix: app-touch-iconの設定

--- src/components/HeadOgp.tsx
+++ src/components/HeadOgp.tsx
@@ -36,18 +36,22 @@ const HeadOgp: FC<Props> = ({ data }) => (
     {/* サムネイル画像の URL */}
     <meta property={'og:image'} content={`${SITE_URL}/img/icon.jpg`} />
     <meta name={'twitter:card'} content={'summary'} />
-    <meta name="msapplication-square70x70logo" content="/site-tile-70x70.png" />
+    <meta name="apple-mobile-web-app-title" content={NAME} />
+    <meta
+      name="msapplication-square70x70logo"
+      content={`${SITE_URL}/img/site-tile-70x70.png`}
+    />
     <meta
       name="msapplication-square150x150logo"
-      content="/site-tile-150x150.png"
+      content={`${SITE_URL}/img/site-tile-150x150.png`}
     />
     <meta
       name="msapplication-wide310x150logo"
-      content="/site-tile-310x150.png"
+      content={`${SITE_URL}/img/site-tile-310x150.png`}
     />
     <meta
       name="msapplication-square310x310logo"
-      content="/site-tile-310x310.png"
+      content={`${SITE_URL}/img/site-tile-310x310.png`}
     />
     <meta name="msapplication-TileColor" content="#0078d7" />
     <link
@@ -59,176 +63,211 @@ const HeadOgp: FC<Props> = ({ data }) => (
     <link
       rel="apple-touch-icon"
       sizes="57x57"
-      href="/apple-touch-icon-57x57.png"
+      href={`${SITE_URL}/img/apple-touch-icon-57x57.png`}
     />
     <link
       rel="apple-touch-icon"
       sizes="60x60"
-      href="/apple-touch-icon-60x60.png"
+      href={`${SITE_URL}/img/apple-touch-icon-60x60.png`}
     />
     <link
       rel="apple-touch-icon"
       sizes="72x72"
-      href="/apple-touch-icon-72x72.png"
+      href={`${SITE_URL}/img/apple-touch-icon-72x72.png`}
     />
     <link
       rel="apple-touch-icon"
       sizes="76x76"
-      href="/apple-touch-icon-76x76.png"
+      href={`${SITE_URL}/img/apple-touch-icon-76x76.png`}
     />
     <link
       rel="apple-touch-icon"
       sizes="114x114"
-      href="/apple-touch-icon-114x114.png"
+      href={`${SITE_URL}/img/apple-touch-icon-114x114.png`}
     />
     <link
       rel="apple-touch-icon"
       sizes="120x120"
-      href="/apple-touch-icon-120x120.png"
+      href={`${SITE_URL}/img/apple-touch-icon-120x120.png`}
     />
     <link
       rel="apple-touch-icon"
       sizes="144x144"
-      href="/apple-touch-icon-144x144.png"
+      href={`${SITE_URL}/img/apple-touch-icon-144x144.png`}
     />
     <link
       rel="apple-touch-icon"
       sizes="152x152"
-      href="/apple-touch-icon-152x152.png"
+      href={`${SITE_URL}/img/apple-touch-icon-152x152.png`}
     />
     <link
       rel="apple-touch-icon"
       sizes="180x180"
-      href="/apple-touch-icon-180x180.png"
+      href={`${SITE_URL}/img/apple-touch-icon-180x180.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="36x36"
-      href="/android-chrome-36x36.png"
+      href={`${SITE_URL}/img/android-chrome-36x36.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="48x48"
-      href="/android-chrome-48x48.png"
+      href={`${SITE_URL}/img/android-chrome-48x48.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="72x72"
-      href="/android-chrome-72x72.png"
+      href={`${SITE_URL}/img/android-chrome-72x72.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="96x96"
-      href="/android-chrome-96x96.png"
+      href={`${SITE_URL}/img/android-chrome-96x96.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="128x128"
-      href="/android-chrome-128x128.png"
+      href={`${SITE_URL}/img/android-chrome-128x128.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="144x144"
-      href="/android-chrome-144x144.png"
+      href={`${SITE_URL}/img/android-chrome-144x144.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="152x152"
-      href="/android-chrome-152x152.png"
+      href={`${SITE_URL}/img/android-chrome-152x152.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="192x192"
-      href="/android-chrome-192x192.png"
+      href={`${SITE_URL}/img/android-chrome-192x192.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="256x256"
-      href="/android-chrome-256x256.png"
+      href={`${SITE_URL}/img/android-chrome-256x256.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="384x384"
-      href="/android-chrome-384x384.png"
+      href={`${SITE_URL}/img/android-chrome-384x384.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="512x512"
-      href="/android-chrome-512x512.png"
+      href={`${SITE_URL}/img/android-chrome-512x512.png`}
     />
-    <link rel="icon" type="image/png" sizes="36x36" href="/icon-36x36.png" />
-    <link rel="icon" type="image/png" sizes="48x48" href="/icon-48x48.png" />
-    <link rel="icon" type="image/png" sizes="72x72" href="/icon-72x72.png" />
-    <link rel="icon" type="image/png" sizes="96x96" href="/icon-96x96.png" />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="36x36"
+      href={`${SITE_URL}/img/icon-36x36.png`}
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="48x48"
+      href={`${SITE_URL}/img/icon-48x48.png`}
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="72x72"
+      href={`${SITE_URL}/img/icon-72x72.png`}
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="96x96"
+      href={`${SITE_URL}/img/icon-96x96.png`}
+    />
     <link
       rel="icon"
       type="image/png"
       sizes="128x128"
-      href="/icon-128x128.png"
+      href={`${SITE_URL}/img/icon-128x128.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="144x144"
-      href="/icon-144x144.png"
+      href={`${SITE_URL}/img/icon-144x144.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="152x152"
-      href="/icon-152x152.png"
+      href={`${SITE_URL}/img/icon-152x152.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="160x160"
-      href="/icon-160x160.png"
+      href={`${SITE_URL}/img/icon-160x160.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="192x192"
-      href="/icon-192x192.png"
+      href={`${SITE_URL}/img/icon-192x192.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="196x196"
-      href="/icon-196x196.png"
+      href={`${SITE_URL}/img/icon-196x196.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="256x256"
-      href="/icon-256x256.png"
+      href={`${SITE_URL}/img/icon-256x256.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="384x384"
-      href="/icon-384x384.png"
+      href={`${SITE_URL}/img/icon-384x384.png`}
     />
     <link
       rel="icon"
       type="image/png"
       sizes="512x512"
-      href="/icon-512x512.png"
+      href={`${SITE_URL}/img/icon-512x512.png`}
     />
-    <link rel="icon" type="image/png" sizes="16x16" href="/icon-16x16.png" />
-    <link rel="icon" type="image/png" sizes="24x24" href="/icon-24x24.png" />
-    <link rel="icon" type="image/png" sizes="32x32" href="/icon-32x32.png" />
-    <link rel="manifest" href="/manifest.json"></link>
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href={`${SITE_URL}/img/icon-16x16.png`}
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="24x24"
+      href={`${SITE_URL}/img/icon-24x24.png`}
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href={`${SITE_URL}/img/icon-32x32.png`}
+    />
+    <link rel="manifest" href={`${SITE_URL}/manifest.json`} />
   </>
 );
 
